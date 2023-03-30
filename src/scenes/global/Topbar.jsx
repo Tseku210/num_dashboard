87612cd16230379f61d8a24a04f88cf45d1af80d
Tseku <otgonbaatartsenguun@yahoo.com>
2023-03-30 16:29:15 +0800

    Add friend request dialog

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,26 +1,74 @@
-import { Box, IconButton, useTheme } from "@mui/material";
-import { useContext } from "react";
+import {
+  Box,
+  Button,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  TextField,
+  useTheme,
+} from "@mui/material";
+import { useContext, useState } from "react";
 import { ColorModeContext, tokens } from "../../theme";
 import InputBase from "@mui/material/InputBase";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
-import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import PersonAddOutlinedIcon from "@mui/icons-material/PersonAddOutlined";
 import SearchIcon from "@mui/icons-material/Search";
 import LoginIcon from "@mui/icons-material/Login";
 import LogoutIcon from "@mui/icons-material/Logout";
 import { Link } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
+import Dialog from "@mui/material/Dialog";
+
+const FriendRequestDialog = (props) => {
+  const { open, onClose } = props;
+
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle>Найзынхаа бүртгэлтэй мэйл хаягыг оруулна уу</DialogTitle>
+      <DialogContent>
+        <Box display="flex" p="10px" gap="5px" alignItems="center">
+          <TextField
+            autoFocus
+            id="name"
+            label="Username"
+            type="email"
+            fullWidth
+            variant="standard"
+          />
+          <Button variant="contained" sx={{ height: "fit-content" }}>
+            Илгээ
+          </Button>
+        </Box>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>Дуусга</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
 
 const Topbar = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const colorMode = useContext(ColorModeContext);
   const { user, logout } = useAuth();
+  const [openFreindRequest, setOpenFreindRequest] = useState(false);
+
+  const handleCloseDialog = () => {
+    setOpenFreindRequest(false);
+  };
 
   const handleLogout = () => {
     logout();
+  };
+
+  const handleFriendRequest = () => {
+    console.log("Friend request sent");
+    setOpenFreindRequest(true);
   };
 
   return (
@@ -37,6 +85,9 @@ const Topbar = () => {
       <Box display="flex">
         {user ? (
           <>
+            <IconButton onClick={handleFriendRequest}>
+              <PersonAddOutlinedIcon />
+            </IconButton>
             <IconButton onClick={colorMode.toggleColorMode}>
               {theme.palette.mode === "dark" ? (
                 <DarkModeOutlinedIcon />
@@ -72,6 +123,10 @@ const Topbar = () => {
         )}
         {/* when clicked it will route to /login */}
       </Box>
+      <FriendRequestDialog
+        open={openFreindRequest}
+        onClose={handleCloseDialog}
+      />
     </Box>
   );
 };
--- src/theme.js
+++ src/theme.js
@@ -197,7 +197,7 @@ export const ColorModeContext = createContext({
 });
 
 export const useMode = () => {
-  const [mode, setMode] = useState("dark");
+  const [mode, setMode] = useState("light");
 
   const colorMode = useMemo(
     () => ({
